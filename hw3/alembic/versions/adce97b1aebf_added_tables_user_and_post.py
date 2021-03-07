17fa806ff6f186567107ccb7ac8c7e8dedeeb9bc
alexfeat <features@bk.ru>
2021-03-07 15:54:19 +0300

    added first migratio

---
+++ hw3/alembic/versions/adce97b1aebf_added_tables_user_and_post.py
@@ -0,0 +1,46 @@
+"""added tables : user and post
+
+Revision ID: adce97b1aebf
+Revises: a7ebe7a84955
+Create Date: 2021-03-07 15:18:04.897689
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'adce97b1aebf'
+down_revision = 'a7ebe7a84955'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.execute('CREATE SCHEMA "user"')
+    op.create_table(
+        'items',
+        sa.Column('id', sa.BIGINT, primary_key=True),
+        sa.Column('username', sa.VARCHAR(32), nullable=False),
+        sa.Column('name', sa.VARCHAR(128)),
+        sa.Column('email', sa.VARCHAR(128), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('username'),
+        schema='user'
+    )
+    op.execute('CREATE SCHEMA "post"')
+    op.create_table(
+        'items',
+        sa.Column('id', sa.BIGINT, primary_key=True),
+        sa.Column('user_id', sa.BIGINT, nullable=False),
+        sa.Column('title', sa.VARCHAR(128), nullable=False),
+        sa.Column('body', sa.Text),
+        sa.PrimaryKeyConstraint('id'),
+        sa.ForeignKeyConstraint(['id'], ['user.items.id']),
+        schema='post',
+    )
+
+def downgrade():
+    op.drop_table('items', schema="post")
+    op.execute('DROP SCHEMA "post"')
+    op.drop_table('items', schema="user")
+    op.execute('DROP SCHEMA "user"')
--- hw3/main.py
+++ hw3/main.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python3
 
-import asyncpg
 import asyncio
+from datetime import datetime as dt
+from asyncpgsa import pg
+from sqlalchemy import (
+    Table,
+    MetaData,
+    Column,
+    Integer,
+    String,
+    DateTime,
+)
+
 
 print('Start!')
 
+metadata = MetaData()
+
+tbl_posts = Table(
+    '"post"."items"',
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer, nullable=False),
+    Column("title", String, nullable=False),
+    Column("body", String, nullable=False, default="", server_default=""),
+    Column("ts_modify", DateTime, nullable=False, default=dt.today().isoformat(), server_default=dt.today().isoformat()),
+)
+
 async def main():
-    conn = await asyncpg.connect(
-        'postgres://uulbmfbi:kwLmeN2XgDLI7kk0iEYrHJ2A5s6Y_up1@dumbo.db.elephantsql.com:5432/uulbmfbi'
+    conn = await pg.init(
+        'postgres://uulbmfbi:kwLmeN2XgDLI7kk0iEYrHJ2A5s6Y_up1@dumbo.db.elephantsql.com:5432/uulbmfbi',
+        min_size=3,
+        max_size=5,
     )
+    post_query = tbl_posts.select()
+
+    results = await pg.query(post_query)
+    for res in results:
+        print(res)
+
     await conn.close()
     return 1
 
